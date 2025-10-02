461f8a612a505f7d56ac4e174f68de699b082602
shahnoorgit <shahnoormujawar@gmail.com>
2025-10-02 18:33:28 +0530

    feat: update build configuration and optimize Metro bundler settings
    - Added gradleCommand to eas.json for specifying the release build command.
    - Enhanced metro.config.js to enable tree shaking and optimize vector icons by excluding unused fonts.
    - Introduced new npm scripts in package.json for bundle analysis and production builds, improving build process efficiency.

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,39 @@ const {
 
 const config = getSentryExpoConfig(__dirname)
 
-module.exports = withNativeWind(config, { input: './app/globals.css' })
+module.exports = withNativeWind(config, { 
+  input: './app/globals.css',
+  // Enable tree shaking for smaller bundles
+  transformer: {
+    minifierConfig: {
+      keep_fnames: true,
+      mangle: {
+        keep_fnames: true,
+      },
+    },
+  },
+  resolver: {
+    // Enable tree shaking
+    unstable_enablePackageExports: true,
+    // Optimize vector icons by excluding unused fonts
+    blockList: [
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/AntDesign\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Entypo\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/EvilIcons\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Feather\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome5_Brands\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome5_Regular\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome5_Solid\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome6_Brands\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome6_Regular\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/FontAwesome6_Solid\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Fontisto\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Foundation\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/MaterialCommunityIcons\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Octicons\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/SimpleLineIcons\.ttf/,
+      /node_modules\/@expo\/vector-icons\/build\/vendor\/react-native-vector-icons\/Fonts\/Zocial\.ttf/,
+    ],
+  },
+})
